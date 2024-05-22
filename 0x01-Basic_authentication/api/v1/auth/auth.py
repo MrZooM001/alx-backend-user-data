787539853ab0cce370839b853fef5a60df205b7c
Hazem Magdy <hhazem0010@gmail.com>
2024-05-22 21:57:05 +0300

    basic auth - tasks to 5

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+from flask import request
+from typing import List, TypeVar
+import fnmatch
+
+
+class Auth:
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if auth is required"""
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        for excluded_path in excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Get authorization header"""
+        if request is not None:
+            return request.headers.get("Authorization", None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Get user from a request"""
+        return None
