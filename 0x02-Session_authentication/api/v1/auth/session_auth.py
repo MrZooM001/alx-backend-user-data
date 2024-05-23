8235fa7f74a93fe67e464bb9861792886f7f3dca
Hazem Magdy <hhazem0010@gmail.com>
2024-05-24 01:48:56 +0300

    Session_authentication - fix task 8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -32,7 +32,7 @@ class SessionAuth(Auth):
             return None
         return self.user_id_by_session_id.get(session_id)
 
-    def current_user(self, request=None) -> User:
+    def current_user(self, request=None):
         """Get user from a request"""
         if request is None:
             return None
@@ -44,19 +44,23 @@ class SessionAuth(Auth):
             return None
         return User.get(user_id)
 
-    def destroy_session(self, request=None) -> bool:
+    def destroy_session(self, request=None):
         """Destroy user session / logout"""
         if request is None:
-            return None
+            return False
+
         session_id = self.session_cookie(request)
         if session_id is None:
             return False
+
         user_id = self.user_id_for_session_id(session_id)
+
         if not user_id:
             return False
+
         try:
             del self.user_id_by_session_id[session_id]
-        except Exception as ex:
+        except Exception:
             pass
 
         return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,29 +4,29 @@
 from os import getenv
 from flask import jsonify, abort, request
 from api.v1.views import app_views
-from models.user import User
 
 
-@app_views.route("/auth_session/login/",
-                 methods=["POST"], strict_slashes=False)
+@app_views.route("/auth_session/login", methods=["POST"], strict_slashes=False)
 def session_login() -> str:
     """POST /api/v1/auth_session/login
     Return:
       - the current logged-in user
     """
     user_email = request.form.get("email")
-    if user_email is None:
+    if not user_email:
         return jsonify({"error": "email missing"}), 400
     user_pwd = request.form.get("password")
-    if user_pwd is None:
+    if not user_pwd:
         return jsonify({"error": "password missing"}), 400
 
     try:
+        from models.user import User
+
         users = User.search({"email": user_email})
     except Exception as ex:
         return jsonify({"error": "no user found for this email"}), 404
 
-    if users is None:
+    if not users:
         return jsonify({"error": "no user found for this email"}), 404
     for user in users:
         if not user.is_valid_password(user_pwd):
@@ -36,14 +36,13 @@ def session_login() -> str:
 
     user = users[0]
     session_id = auth.create_session(user.id)
-    session_name = getenv("SESSION_NAME")
     respo = jsonify(user.to_json())
+    session_name = getenv("SESSION_NAME")
     respo.set_cookie(session_name, session_id)
     return respo
 
 
-@app_views.route("/auth_session/logout/",
-                 methods=["DELETE"], strict_slashes=False)
+@app_views.route("/auth_session/logout", methods=["DELETE"], strict_slashes=False)
 def session_logout():
     """DELETE /api/v1/auth_session/logout
     Return:
