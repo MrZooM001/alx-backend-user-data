fd005086bfb96caf1b6a875f605b64778f125290
Hazem Magdy <hhazem0010@gmail.com>
2024-05-30 16:19:55 +0300

    user auth service - fix task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,11 +49,11 @@ def logout() -> str:
     """logout endpoint"""
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
-        AUTH.destroy_session(user.id)
-        return redirect("/")
-    else:
+    if not user:
         abort(403)
+
+    AUTH.destroy_session(user.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
