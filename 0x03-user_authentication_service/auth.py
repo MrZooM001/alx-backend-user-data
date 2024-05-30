4b3981260dd20031458edd28c04171889a6b8214
Hazem Magdy <hhazem0010@gmail.com>
2024-05-30 16:28:15 +0300

    user auth service - fix2 task 14

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,4 +73,4 @@ class Auth:
             user = self._db.find_user_by(id=user_id)
             self._db.update_user(user.id, session_id=None)
         except NoResultFound:
-            return
+            pass
